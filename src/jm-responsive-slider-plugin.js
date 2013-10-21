ee6dca20904e87f94e4f115332229bb162087743
jmsv23 <jmsv23@gmail.com>
2013-10-21 13:03:29 -0500

    first commit

---
+++ src/jm-responsive-slider-plugin.js
@@ -0,0 +1,117 @@
+(function($) {
+    $.fn.jmResponsiveSlider = function(options) {
+
+       // Establish our default settings
+        var settings = $.extend({
+            tiempo       : 500,
+            breakpoint   : null,
+            evento       : 'click',
+            onmove       : null,
+            complete     : null
+        }, options);
+
+        return this.each( function() {
+            // variables set
+            var container = $(this);
+            var n = container.children('.slidecontent').children('.slide').size();
+            var contenedor = container.children('.slidecontent');
+            var slide = contenedor.children('.slide');
+            var size = container.outerWidth(false);
+            var avance = 0;
+            var widthState = $( window ).width();
+            var lock = false;
+            container.css('position', 'relative');
+            contenedor.width(size * n);
+
+            //function de control de breakpoint
+            function checkBreakpoint( bp, tam ) {
+                //if breakpoint exist set inilial config
+                if ($.isArray( bp )) {
+                    if ( bp.length > 1 ) {
+                        if( tam > bp[0].bp && tam < bp[1].bp ) {
+                            return bp[0].slides;
+                        } else if ( tam > bp[1].bp ) {
+                            return bp[1].slides;
+                        } else {
+                            return 1;
+                        }
+                    } else if( tam > bp[0].bp) {
+                        return bp[0].slides;
+                    } else {
+                        return 1;
+                    }
+                } else {
+                    return 1;
+                }
+            }
+
+            //asignando numero de elementos por slides dependiendo de los break point's
+            var num = checkBreakpoint( settings.breakpoint, size );
+            // ancho del contenedor de slides
+            contenedor.width(size * (Math.ceil(n / num)));
+            slide.width(size / num);
+
+
+       		//create the controls
+       		var izq = $('<span class="n-controls izq"></span>');
+       		var der = $('<span class="n-controls der"></span>');
+
+       		izq.insertAfter(contenedor);
+       		der.insertAfter(contenedor);
+            
+            $('.n-controls').on(settings.evento, function () {
+                if(!lock) {
+                    var total = contenedor.width();
+                    if ( $(this).hasClass('izq') ) {
+                        if ( avance < 0 ) {
+                            lock = true;//evita doble interaccion
+                            //funcion se dispara en evento si existe
+                            if ( $.isFunction( settings.onmove ) ) {
+                                settings.onmove.call( this );
+                            }
+                            avance += size;
+                            contenedor.animate({'margin-left': avance}, settings.tiempo, function() { lock = false});
+                        }
+                    }
+
+                    if ( $(this).hasClass('der') ) {
+                        if ( avance > - (total - size) ) {
+                            lock = true;
+                            //funcion se dispara en evento si existe
+                            if ( $.isFunction( settings.onmove ) ) {
+                                settings.onmove.call( this );
+                            }
+                            avance -= size;
+                            contenedor.animate({'margin-left': avance}, settings.tiempo, function() { lock = false});
+                        }
+                    }
+                }
+            });
+            
+            //when window resize
+            $( window ).resize(function() {
+            	if ( widthState !== $( window ).width() ) {
+                    size = container.outerWidth(false);
+                    contenedor.css('margin-left', 0);
+                    avance = 0;
+
+                    //asignando numero de elementos por slides dependiendo de los break point's
+                    var num = checkBreakpoint( settings.breakpoint, size );
+                    // ancho del contenedor de slides
+                    contenedor.width(size * (Math.ceil(n / num)));
+                    slide.width(size / num);
+                } 
+                //control si ancho cambia
+                widthState = $( window ).width();
+            });
+            
+
+            //calback complete
+            if ( $.isFunction( settings.complete ) ) {
+                settings.complete.call( this );
+            }
+        });
+
+    }
+
+}(jQuery));
